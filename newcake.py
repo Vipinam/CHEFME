4db0dbca41c3ba2d43eb7250946c53fbf07fa542
Vipinam <vipinasumesh007@gmail.com>
2025-07-03 14:29:51 +0530

    Add files via upload

---
+++ newcake.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("JOIN AN INTERACTIVE LIVE CLASS WITH CHEF RISHI!")
+st.write("bake ur information below")
+name=st.text_input("Enter your name:")
+if st.button("submit:)"):
+   st.write(f" Nice to meet u ,(name),lets join an experience to bake a cake!.")
