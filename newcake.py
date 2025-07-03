27524706a77021a88ac63f09bc1f8d0e8d5c56dc
Vipinam <vipinasumesh007@gmail.com>
2025-07-03 14:34:47 +0530

    Add files via upload

--- newcake.py
+++ newcake.py
@@ -3,4 +3,4 @@ st.title("JOIN AN INTERACTIVE LIVE CLASS WITH CHEF RISHI!")
 st.write("bake ur information below")
 name=st.text_input("Enter your name:")
 if st.button("submit:)"):
-   st.write(f" Nice to meet u ,(name),lets join an experience to bake a cake!.")
+   st.write(f" Nice to meet u ,{name},lets join an experience to bake a cake!.")
